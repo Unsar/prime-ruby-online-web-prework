dd38742d14f8217f52b9c0786a16122ea6285e3c
Unsar Malik <unsar.malik24@gmail.com>
2019-04-09 17:12:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 # Add  code here!
  def prime?(num)
   (2..(num - 1)).each do |n|
-    return false if (num % n == 0 || n > 0)
+    return false if num % n == 0
+    elsif if num < 0 return false
   end
   true
 end
