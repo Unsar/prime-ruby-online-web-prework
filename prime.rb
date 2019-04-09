93a6db238418de2e82e123da2e896f470fbf2734
Unsar Malik <unsar.malik24@gmail.com>
2019-04-09 17:00:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
  def is_prime n
   foundDivider = false
    for d in 2..(n - 1)
-    foundDivider = ((n % d) == 0) or foundDivider
+    foundDivider = ((n % d) == 0 || foundDivider
    end
   not foundDivider
  end
