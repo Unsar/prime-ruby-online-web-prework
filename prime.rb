2c2a0cd9cbc892e912587ad70301b0730439ff60
Unsar Malik <unsar.malik24@gmail.com>
2019-04-09 17:21:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,3 @@
 # Add  code here!
-def prime? max
-    primes = []
-    (1..max).each {|num| 
-        if
-            (2..num-1).all? {|denom| num%denom >0}
-        then
-            primes.push(num) 
-            puts true
-        end
-    }
-    puts primes
-end
+def prime? 
 
